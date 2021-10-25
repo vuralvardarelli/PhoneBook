2a593b391bc1d9034388bb657de86772188fd4dd
Vural Vardarelli <vuralvardarelli@hotmail.com>
2021-10-26 01:41:29 +0300

    Add auto db and tables creation to Startup.cs

--- src/RepositoryService/RepositoryService.API/Startup.cs
+++ src/RepositoryService/RepositoryService.API/Startup.cs
@@ -40,6 +40,12 @@ namespace RepositoryService.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<PhonebookContext>();
+                context.Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
